9ffac7853aae8871faa5b50e391740ff2a5afca2
sebabonvehi <sebastianbonvehi@hotmail.com>
2022-04-06 20:59:22 -0300

    Entrada_Salida info y ejercicio

--- Entrada-Salida/src/Entrada_Salida.java
+++ Entrada-Salida/src/Entrada_Salida.java
@@ -1,82 +1,161 @@
 import java.io.*;
+import java.util.HashMap;
+//import java.util.Scanner;
 
 public class Entrada_Salida {
 	
-	/*
-	 * static = accede al metodo sin necesidad de referenciar o instanciar la clase
-	 * 
-	 * Stream = flujo de una uente a un destino. Trabaja la comunicacion 
-	 * de informacion. Estos flujos son secuencias ordenadas de datos 
-	 * que tienen un flujo de entrada o un flujo de salida
-	 * 
-	 * Flujos de bytes = los flujos de entrada estan implementados por subclases de la clase InputStream 
-	 * y los de salida OutputStream.
-	 * 
-	 * Flujo de Caracteres = los flujos de entrada de caracteres estan implementado por subclases de la clase Reader 
-	 * y los de saluda Writer.
-	 * 
-	 * Clase abstract = no puedo generar una instancia de esta clase, pero si de las que heredan de esta.
-	 * 
-	 * InputStream:
-	 * 
-	 * read() lee un caracter y retorna un entero
-	 * 
-	 * read(byte[] buffer) lee el flujo en un arreglo y devuelve la cantidad de bytes leidos
-	 * 
-	 * read(byte[] buffer, int offset, int lenght) desde que inicio y hasta que fin leer los bytes y devuelve la cant de bytes leidos
-	 * 
-	 * close() cierra el flujo
-	 * 
-	 * skip(long n) descarta la cant de bytes del flujo
-	 * 
-	 * OutputStream:
-	 * 
-	 * write()
-	 * 
-	 * write(byte[] buffer)
-	 * 
-	 * write(byte[] buffer, int offset, int lenght)
-	 * 
-	 * close() cierra el flujo
-	 * 
-	 * flush() vacia el flujo
-	 * 
-	 * -------------------------------------------------------------------------------------------------------------------------------
-	 * 
-	 * Reader:
-	 * 
-	 * read() lee un caracter y retorna un entero
-	 * 
-	 * read(byte[] buffer) lee el flujo en un arreglo y devuelve la cantidad de bytes leidos
-	 * 
-	 * read(byte[] buffer, int offset, int lenght) desde que inicio y hasta que fin leer los bytes y devuelve la cant de bytes leidos
-	 * 
-	 * close() cierra el flujo
-	 * 
-	 * skip(long n) descarta la cant de bytes del flujo
-	 * 
-	 * Writer:
-	 * 
-	 * write(int b) escribe en el flujo de salido los 8 bits de menor orden del argumento b
-	 * 
-	 * write(byte[] b)
-	 * 
-	 * write(byte[] b, int offset, int lenght)
-	 * 
-	 * close() cierra el flujo
-	 * 
-	 * flush() vacia el flujo
-	 */
-	
-	public static void main(String[] args) throws IOException  {
+	private final static String MAX = "Maximo";
+	private final static String MIN = "Minimo";
+	private final static String PROM = "Promedio";
 		
-		leerDeTeclado();
-		leerEnteros();
-		leerUsandoSplit(args);
-		escribirArchivo();
-		escribirLineasEnArchivo();
+	public static void main(String[] args) throws IOException  {
+//		leerDeTeclado();
+//		leerEnteros();
+//		leerUsandoSplit(args);
+//		escribirArchivo();
+//		escribirLineasEnArchivo();
+		obtenerMaxMinYProm();
 	}
 
+	private static void obtenerMaxMinYProm() throws IOException {
+		int nrosEscritosEnArchivo = escribirArchivoNumRand();
+		HashMap<String, Integer> hash = leerEnteros("nrosRandom.txt", nrosEscritosEnArchivo);
+		escribirArchivoResultado(hash);
+	}
+
+	private static void escribirArchivoResultado(HashMap<String, Integer> hash) {
+		System.out.println("+----------+-------+");
+		System.out.println("| Máximo   | " + hash.get(MAX) + " |");
+		System.out.println("+----------+-------+");
+		System.out.println("| Minimo   |" + String.format("%5s", " ") + hash.get(MIN) + " |");
+		System.out.println("+----------+-------+");
+		System.out.println("| Promedio |" + String.format("%2s", " ") + hash.get(PROM) + String.format("%1s", " ") + "|");
+		System.out.println("+----------+-------+");
+	}
+
+	private static int escribirArchivoNumRand() {
+		int min = 0;
+		int max = 12000;
+		int cantNrosAleatoriosMin = 10000;
+		int cantNrosAleatoriosMax = 20000;
+		
+		int nrosAleatorios = (int)(Math.random() * (cantNrosAleatoriosMax - cantNrosAleatoriosMin + 1) + cantNrosAleatoriosMin);
+		escribirLineasEnArchivo(min, max, nrosAleatorios);
+		return nrosAleatorios;
+	}
+
+	private static void escribirLineasEnArchivo(int min, int max, int nrosAleatorios) {
+		
+		FileWriter fichero = null;
+		PrintWriter pw = null;
+		
+		try {
+			
+			fichero = new FileWriter("nrosRandom.txt");
+			pw = new PrintWriter(fichero);
+			
+			int i = 0;
+			int nro;
+			
+			while(i < nrosAleatorios) {
+				nro = (int)(Math.random() * (max - min + 1) + min);
+				pw.println(nro);
+				i++;
+			}
+			
+			
+		} catch(Exception e) {
+			
+			e.printStackTrace();
+			
+		} finally {
+			try {
+				if(null != fichero)
+					fichero.close();
+			} catch(Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+	}
+
+	private static HashMap<String, Integer> leerEnteros(String nombreArch, int nrosEscritosEnArchivo) throws IOException {
+		
+		File f = null;
+		FileReader fr = null;
+		BufferedReader br = null;
+		
+		int numero;
+		int max = 0;
+		int min = 0;
+		int prom = 0;
+		int i = 0;
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		
+		try {
+			
+			f = new File(nombreArch);
+			fr = new FileReader(f);
+			br = new BufferedReader(fr);
+			String linea;
+			
+			while((linea = br.readLine()) != null) {
+				
+				numero = Integer.parseInt(linea);
+				
+				if(numero > max)
+					max = numero;
+				
+				if(i == 0 || numero < min)
+					min = numero;
+				
+				prom += numero;
+				
+				i++;
+			}
+			
+			prom /= nrosEscritosEnArchivo;
+			
+			
+			
+			map.put(MAX, max);
+			map.put(MIN, min);
+			map.put(PROM, prom);
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			br.close();
+		}
+		
+		return map;
+	}
+	
+/*	private static void leerUsandoSplit(String[] args) throws IOException {
+		
+		try {
+			FileReader input = new FileReader(args[0]);
+			BufferedReader br = new BufferedReader(input);
+			String line;
+			line = br.readLine();
+			
+			while (line != null) {
+				String[] datos;
+				datos = line.split(" ");
+				int entero1 = Integer.parseInt(datos[0]);
+				int entero2 = Integer.parseInt(datos[1]);
+				System.out.println(entero1 + " " + entero2);
+				line = br.readLine();
+			}
+			
+			br.close();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	
 	private static void escribirLineasEnArchivo() {
 		FileWriter fichero = null;
 		PrintWriter pw = null;
@@ -124,58 +203,6 @@ public class Entrada_Salida {
 		}
 	}
 
-	private static void leerUsandoSplit(String[] args) throws IOException {
-		
-		try {
-			FileReader input = new FileReader(args[0]);
-			BufferedReader br = new BufferedReader(input);
-			String line;
-			line = br.readLine();
-			
-			while (line != null) {
-				String[] datos;
-				datos = line.split(" ");
-				int entero1 = Integer.parseInt(datos[0]);
-				int entero2 = Integer.parseInt(datos[1]);
-				System.out.println(entero1 + " " + entero2);
-				line = br.readLine();
-			}
-			
-			br.close();
-		} catch(IOException e) {
-			e.printStackTrace();
-		}
-		
-		
-		
-	}
-
-	private static void leerEnteros() throws IOException {
-		File f = null;
-		FileReader fr = null;
-		BufferedReader br = null;
-		
-		int numero;
-		
-		try {
-			
-			f = new File("enteros.txt");
-			fr = new FileReader(f);
-			br = new BufferedReader(fr);
-			String linea;
-			
-			while((linea = br.readLine()) != null) {
-				numero = Integer.parseInt(linea);
-				System.out.print(numero);
-			}
-			
-			br.close(); //implementarlo en otro lado
-			
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-
 	private static void leerDeTeclado() throws IOException {
 		int c;
 		int contador = 0;
@@ -188,15 +215,6 @@ public class Entrada_Salida {
 		}
 		
 		System.out.println("\nContados " + contador + " bytes en total.");
-	}
+	} */
 	
-	/*
-	 * BufferedReader/BufferedWriter para flujo de caracteres
-	 * BufferedInputStream/BufferedOutputStream para flujo de bytes
-	 * Ejemplo: 
-	 * InputStreamReader entrada = new InputStreamReader(System.in);
-	 * BufferedReader teclado = new BufferedReader(entrada);
-	 * String cadena = teclado.readLine();
-	 */
-
 }
